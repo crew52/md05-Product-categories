255d1c9e18fd056154f5088a11137fe0d6857e48
crew52 <dungnc150602@gmail.com>
2025-04-04 10:06:34 +0700

    done all

--- product/src/App.jsx
+++ product/src/App.jsx
@@ -1,9 +1,5 @@
 import {Route, Routes} from "react-router";
 import './App.css'
-import ProductList from "./pages/productlist/index.jsx";
-import ProductCreate from "./pages/productcreate/index.jsx";
-import ProductEdit from "./pages/productedit/index.jsx";
-import ProductDetail from "./pages/productdetail/index.jsx";
 import MusicList from "./pages/music/musicList/index.jsx";
 import MusicCreate from "./pages/music/musicCreate/index.jsx";
 
@@ -11,11 +7,6 @@ function App() {
   return (
     <>
         <Routes>
-            <Route path={"/products"} element={<ProductList/>}></Route>
-            <Route path="/products/create" element={<ProductCreate/>} />
-            <Route path="/products/:pid/edit" element={<ProductEdit/>} />
-            <Route path="/products/:pid/detail" element={<ProductDetail/>} />
-
             <Route path={"/musics"} element={<MusicList/>}></Route>
             <Route path="/musics/create" element={<MusicCreate/>} />
         </Routes>
--- product/src/services/category.service.js
+++
@@ -1,9 +0,0 @@
-import {axiosInstance} from "../configs/axios.config.js";
-
-class CategoryService{
-    static async getAllCategories() {
-        return await axiosInstance.get("/categories")
-    }
-}
-
-export default CategoryService;
